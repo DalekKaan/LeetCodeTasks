f259462a79e63a6ac23a7e5262ea15148dfe6b80
Alexey Seleznev <rib@live.ru>
2025-04-15 01:56:17 +0300

    task 848 done

--- src/test/java/ru/r1b/solution/task848/SolutionTest.java
+++ src/test/java/ru/r1b/solution/task848/SolutionTest.java
@@ -12,12 +12,12 @@ class SolutionTest {
 
     public static Stream<Arguments> source() {
         return Stream.of(
-//                Arguments.of(
-//                        "abc", new int[]{3, 5, 9}, "rpl"
-//                ),
-//                Arguments.of(
-//                        "ruu", new int[]{26,9,17}, "rul"
-//                ),
+                Arguments.of(
+                        "abc", new int[]{3, 5, 9}, "rpl"
+                ),
+                Arguments.of(
+                        "ruu", new int[]{26,9,17}, "rul"
+                ),
                 Arguments.of(
                         "mkgfzkkuxownxvfvxasy", new int[]{505870226,437526072,266740649,224336793,532917782,311122363,567754492,595798950,81520022,684110326,137742843,275267355,856903962,148291585,919054234,467541837,622939912,116899933,983296461,536563513}, "wqqwlcjnkphhsyvrkdod"
                 )
