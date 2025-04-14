a85cc52d29038db81e015f7aa070e51524ee3924
Alexey Seleznev <rib@live.ru>
2025-04-15 02:18:29 +0300

    task 848 variant

--- src/main/java/ru/r1b/solution/task848/Solution.java
+++ src/main/java/ru/r1b/solution/task848/Solution.java
@@ -5,18 +5,13 @@ package ru.r1b.solution.task848;
  */
 public class Solution {
     public String shiftingLetters(String s, int[] shifts) {
-        long[] prefSum = new long[shifts.length];
-        prefSum[shifts.length - 1] = shifts[shifts.length - 1];
-        for (int i = shifts.length - 1; i > 0; i--) {
-            prefSum[i-1] = prefSum[i] + shifts[i - 1];
-        }
-
         char[] chars = s.toCharArray();
 
-        for (int i = 0; i < chars.length; i++) {
+        int toShift = 0;
+        for (int i = chars.length - 1; i >= 0; i--) {
+            toShift = (toShift + shifts[i]) % 26;
             char ch = chars[i];
-            long prs = prefSum[i];
-            chars[i] = (char) ((ch - 'a' + prs) % 26 + 'a');
+            chars[i] = (char) ((ch - 'a' + toShift) % 26 + 'a');
         }
         return String.valueOf(chars);
 
