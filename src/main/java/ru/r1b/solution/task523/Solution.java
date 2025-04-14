18f9700dbb86556d8957abe8890db46f1fd837f3
Alexey Seleznev <rib@live.ru>
2025-04-15 00:48:29 +0300

    task 523 closed

--- src/main/java/ru/r1b/solution/task523/Solution.java
+++ src/main/java/ru/r1b/solution/task523/Solution.java
@@ -1,38 +1,24 @@
 package ru.r1b.solution.task523;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 523. Continuous Subarray Sum
  */
 public class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
-        if (nums.length == 1) {
-            return false;
-        }
-        int sum = 0, prev = -1;
-        for (int n : nums) {
-            if (prev != -1 && (prev + n) % k == 0) {
-                return true;
-            }
-            sum += n;
-            prev = n;
-        }
-        if (sum > 0 && sum< k) {
-            return false;
-        }
-        if (sum % k == 0) {
-            return true;
-        }
-        for (int i = 0; i < nums.length - 1; i++) {
-            int temp = sum;
-            for (int j = nums.length - 1; j > i + 1; j--) {
-                temp -= nums[j];
-                if (temp % k == 0) {
+        // stolen https://github.com/AnatoliySokolov98/LeetCode/blob/main/0523/solution.java
+        Map<Integer, Integer> mods = new HashMap<>();
+        mods.put(0, -1);
+        int total = 0;
+        for (int i = 0; i < nums.length; i++) {
+            total = (total + nums[i]) % k;
+            if (mods.containsKey(total)) {
+                if (i - mods.get(total) > 1)
                     return true;
-                }
-            }
-            sum -= nums[i];
-            if (i < nums.length - 2 && sum % k == 0) {
-                return true;
+            } else {
+                mods.put(total, i);
             }
         }
         return false;
