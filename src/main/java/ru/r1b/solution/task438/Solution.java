19b4cd8f47f84387bab3232c148cf1f13e479349
Alexey Seleznev <rib@live.ru>
2025-04-14 17:54:15 +0300

    task 438 done

--- src/main/java/ru/r1b/solution/task438/Solution.java
+++ src/main/java/ru/r1b/solution/task438/Solution.java
@@ -3,31 +3,43 @@ package ru.r1b.solution.task438;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * 438. Find All Anagrams in a String
+ */
 public class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         var out = new LinkedList<Integer>();
+        if (p.length()> s.length()) {
+            return out;
+        }
         char[] chars1 = s.toCharArray(), chars2 = p.toCharArray();
-        int[] freq = new int[26];
+        int[] baseFreq = new int[26], floatFreq = new int[26];
 
-        int l = 0, r = chars2.length-1;
+        for (int i = 0; i < chars2.length; i++) {
+            baseFreq[chars2[i] - 'a']++;
+            floatFreq[chars1[i] - 'a']++;
+        }
+        int l = 0, r = chars2.length - 1;
         while (r < chars1.length) {
-            for (int i = l; i <= r; i++) {
-                freq[chars1[i] - 'a']++;
-                freq[chars2[i-l] - 'a']--;
-            }
             boolean isAnagram = true;
-            for (int i = 0; i < freq.length; i++) {
-                int f = freq[i];
-                if (f > 0) {
+            for (int i = 0; i < floatFreq.length; i++) {
+                if (floatFreq[i] != baseFreq[i]) {
                     isAnagram = false;
+                    break;
                 }
-                freq[i] = 0;
             }
             if (isAnagram) {
                 out.add(l);
             }
-            l++;
-            r++;
+
+            if (r != chars1.length - 1) {
+                floatFreq[chars1[l] - 'a']--;
+                l++;
+                r++;
+                floatFreq[chars1[r] - 'a']++;
+            } else {
+                break;
+            }
         }
 
 
